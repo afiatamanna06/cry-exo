e027079cf703485f8d0110c25078c4f33f113f1c
afiatamanna06 <afiatamanna06@gmail.com>
2023-03-20 10:12:19 +0600

    Update navigation bar file

--- components/navigation/NavigationBar.tsx
+++ components/navigation/NavigationBar.tsx
@@ -1,9 +1,105 @@
-import React from 'react'
+import {
+  Box,
+  Flex,
+  Image,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  useDisclosure,
+} from "@chakra-ui/react";
+import Link from "next/link";
+//import { useAccount, useDisconnect, useEnsName, useBalance } from "wagmi";
 
-const NavigationBar = () => {
+function NavigationBar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  /*const { address, connector, isConnected } = useAccount();
+    const { data: ensName } = useEnsName({ address });
+    const { disconnect } = useDisconnect();
+    const {
+      data: Balance,
+      isError,
+      isLoading,
+    } = useBalance({
+      address:
+        ensName !== null && ensName !== undefined && address !== undefined
+          ? `${address}`
+          : address,
+    });
+    console.log(Balance);*/
+
   return (
-    <div>NavigationBar</div>
-  )
+    <Flex
+      justify="space-between"
+      fontFamily="'IBM Plex Mono', monospace"
+      borderBottom="1px"
+      borderColor="rgba(168, 162, 158, 1)"
+      bg="rgba(245, 245, 245, 1)"
+      alignItems="center"
+      w="full"
+      py={["4"]}
+      px={["6", "6", "8", "8"]}
+    >
+      <Link href="/" passHref>
+        <Box>CryExo</Box>
+      </Link>
+      <Box>
+        {/*{isConnected ? (
+            <Flex alignItems="center" gap={[2, 2, 3, 3]}>
+              <Box fontWeight="semibold" fontSize={["xs", "xs", "sm", "md"]}>
+                {isLoading && <Box>Fetching balance…</Box>}
+                {isError && <Box>Error fetching balance</Box>}
+                <Flex gap={1}>
+                  <Box display={["none", "none", "block", "block"]}>
+                    Your balance:{" "}
+                  </Box>
+                  <Box>{`${Balance?.formatted} ${Balance?.symbol}`}</Box>
+                </Flex>
+              </Box>
+              <Box>
+                <Menu>
+                  <MenuButton>
+                    <CommonButton
+                      name={
+                        ensName
+                          ? `${ensName} (${address?.slice(
+                              0,
+                              8
+                            )}..${address?.slice(
+                              address.length - 4,
+                              address.length
+                            )})`
+                          : `${address?.slice(0, 5)}..${address?.slice(
+                              address.length - 5,
+                              address.length
+                            )}`
+                      }
+                      bg="white"
+                      hoverBg="rgba(245, 245, 245, 1)"
+                    />
+                  </MenuButton>
+                  <MenuList borderRadius="sm" border="1px">
+                    <MenuItem fontSize="sm" onClick={() => disconnect()}>
+                      disconnect
+                    </MenuItem>
+                  </MenuList>
+                </Menu>
+              </Box>
+            </Flex>
+          ) : (
+            <Flex direction="column">
+              <CommonButton
+                name="Connect your wallet"
+                onClick={onOpen}
+                bg="rgba(187, 247, 208, 1)"
+                hoverBg="rgba(74, 222, 128, 1)"
+              />
+              
+            </Flex>
+          )}*/}
+      </Box>
+    </Flex>
+  );
 }
 
-export default NavigationBar
+export default NavigationBar;
