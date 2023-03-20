4d9c3f141e2ec54dcb43672dfb3165386a7d7614
afiatamanna06 <afiatamanna06@gmail.com>
2023-03-20 10:06:08 +0600

    Add navigation bar file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,19 @@
-import './globals.css'
-
-export const metadata = {
-  title: 'CryExo',
-  description: 'A crypto trading platform',
-}
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>CryExo</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="description" content="A crypto trading platform" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
