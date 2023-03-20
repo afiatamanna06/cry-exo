92aae9c70d235f27ded073ddcd7295876898fae9
afiatamanna06 <afiatamanna06@gmail.com>
2023-03-20 09:42:44 +0600

    Add app title

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,13 @@
 
 import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider } from '@chakra-ui/react'
-import { Box, Button } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <main>
       <CacheProvider>
         <ChakraProvider>
-          <Button>Meh</Button>
+          
         </ChakraProvider>
       </CacheProvider>
     </main>
