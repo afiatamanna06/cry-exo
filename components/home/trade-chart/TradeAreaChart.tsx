87a165f1fc443428b4932abfd91f21c147f97527
afiatamanna06 <afiatamanna06@gmail.com>
2023-03-29 20:02:55 +0600

    Update chart labelling

--- components/home/trade-chart/TradeAreaChart.tsx
+++ components/home/trade-chart/TradeAreaChart.tsx
@@ -12,109 +12,109 @@ import {
 const TradeAreaChart = () => {
   const data = [
     {
-      time: "03:00",
+      time: "03:00 AM",
       Yesterday: 1105,
       Today: 1390,
       money: 1300,
     },
     {
-      time: "04:00",
+      time: "04:00 AM",
       Yesterday: 1703,
       Today: 1420,
       money: 1450,
     },
     {
-      time: "05:00",
+      time: "05:00 AM",
       Yesterday: 1367,
       Today: 1530,
       money: 1500,
     },
     {
-      time: "06:00",
+      time: "06:00 AM",
       Yesterday: 1643,
       Today: 1200,
       money: 1600,
     },
     {
-      time: "07:00",
+      time: "07:00 AM",
       Yesterday: 1690,
       Today: 1645,
       money: 1700,
     },
     {
-      time: "08:00",
+      time: "08:00 AM",
       Yesterday: 1766,
       Today: 1990,
       money: 1800,
     },
     {
-      time: "09:00",
+      time: "09:00 AM",
       Yesterday: 1023,
       Today: 1790,
       money: 1900,
     },
     {
-      time: "10:00",
+      time: "10:00 AM",
       Yesterday: 1809,
       Today: 1710,
       money: 2000,
     },
     {
-      time: "11:00",
+      time: "11:00 AM",
       Yesterday: 690,
       Today: 1345,
       money: 1700,
     },
     {
-      time: "12:00",
+      time: "12:00 PM",
       Yesterday: 1766,
       Today: 2590,
       money: 1800,
     },
     {
-      time: "13:00",
+      time: "13:00 PM",
       Yesterday: 1023,
       Today: 1790,
       money: 1900,
     },
     {
-      time: "14:00",
+      time: "14:00 PM",
       Yesterday: 1809,
       Today: 1410,
       money: 3000,
     },
     {
-      time: "15:00",
+      time: "15:00 PM",
       Yesterday: 1690,
       Today: 1345,
       money: 1700,
     },
     {
-      time: "16:00",
+      time: "16:00 PM",
       Yesterday: 2166,
       Today: 2890,
       money: 1800,
     },
     {
-      time: "17:00",
+      time: "17:00 PM",
       Yesterday: 1023,
       Today: 1790,
       money: 1900,
     },
     {
-      time: "18:00",
+      time: "18:00 PM",
       Yesterday: 1809,
       Today: 1410,
       money: 3000,
     },
     {
-      time: "19:00",
+      time: "19:00 PM",
       Yesterday: 690,
       Today: 1745,
       money: 1700,
     },
     {
-      time: "20:00",
+      time: "20:00 PM",
       Yesterday: 1766,
       Today: 2590,
       money: 1800,
@@ -142,7 +142,7 @@ const TradeAreaChart = () => {
         margin={{
           top: 5,
           right: 5,
-          left: -15,
+          left: -5,
           bottom: 5,
         }}
       >
@@ -174,7 +174,7 @@ const TradeAreaChart = () => {
         </defs>
         <CartesianGrid strokeDasharray="5 5" />
         <XAxis dataKey="time" />
-        <YAxis dataKey="money" />
+        <YAxis dataKey="money" unit="$" />
         <Tooltip />
         <Legend wrapperStyle={{ lineHeight: "40px" }} />
         <Area
